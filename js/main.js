e5e91afa2638cd61cc46112cde431884b9a2bcc9
Ivannushka <ivannaorlov@gmail.com>
2021-03-25 22:05:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ function changeColor() {
     const h2ColorChange = document.getElementById('h2-color-change');
     h2ColorChange.style.color ='red';
      h2ColorChange.style.fontSize = '50px';
+     h2ColorChange.style.fontFamily = "Arial, Helvetica, sans-serif";
 }
