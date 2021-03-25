546c83986afadabd84cabbac1ec3667b2f04e80a
Ivannushka <ivannaorlov@gmail.com>
2021-03-25 22:00:47 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function changeColor() {
+    const h2ColorChange = document.getElementById('h2-color-change');
+    h2ColorChange.style.color ='red';
+     h2ColorChange.style.fontSize = '50px';
+}
